7adc2b4fac74a9cd4919b38be547b18a72da031f
Turu0512 <hinataptyan@gmail.com>
2021-08-01 12:06:39 +0900

    btnの設定

--- vuetify2/src/App.vue
+++ vuetify2/src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" dark app>
+    <v-navigation-drawer v-model="drawer" clipped app>Navigation Lists</v-navigation-drawer>
+    <!-- 画面サイズによってサイドバーが表示・非表示される。ユーザーで操作するためにハンバーガーメニューを作る -->
+    <!-- clippedを設定することで、ナビゲーションバーの下に表示する。-->
+    <v-app-bar color="primary" clipped-left dark app>
       <!-- darkはvuetify上で用意されたテーマ。appを設定すると自動で高さを調節してくれる -->
       <!-- colorは背景色。color=色--textで文字色「-」は二個 -->
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <!-- ハンバーガーメニュー,drawerは提議していなくてもドキュメントに記述されているため、dataで定義すれば扱える -->
       <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- 次の要素との間を開ける -->
+      <!-- <v-btn text>button</v-btn> -->
+      <!-- textを設定すると背景と同じ色になって、結果透明に見える -->
+      <!-- <v-btn outlined>button</v-btn> -->
+      <!-- outlinedで枠線だけの表示 -->
+      <v-toolbar-items>
+      <v-btn text>FOR ENTERPRISE</v-btn>
+      <v-btn text>SUPPORT</v-btn>
+      </v-toolbar-items>
+      <!-- v-toolbar-itemsに入れることで、バーいっぱいに選択領域が拡大する -->
+
     </v-app-bar>
     <v-footer dark app>Vuetify</v-footer>
   </v-app>
@@ -12,13 +29,10 @@
 <script>
 export default {
   name: 'App',
-
-  components: {
-    
+  data(){
+    return{
+      drawer: null
+    }
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
