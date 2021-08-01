7f06ffda0bfb977a867714005a83691490bb40c6
Turu0512 <hinataptyan@gmail.com>
2021-08-01 17:35:18 +0900

    vue-routerの追加

--- vuetify2/src/App.vue
+++ vuetify2/src/App.vue
@@ -1,6 +1,41 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" clipped app>Navigation Lists</v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" clipped app>
+      <v-container>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title grey--text text--darken-2">
+            <!-- titleをつけると大きくなる -->
+            Navigation Lists
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <!-- 線を引く -->
+
+      <v-list dense nav>
+        <!-- dense（密集）は空白グリッドを埋めてパッキングをするようにグリッドアイテムが自動配置される。 
+        グリッドアイテムのサイズの大小をもとにパッキングの順番が決められる。 -->
+        <v-list-group v-for="nav_list in nav_lists" 
+        :key="nav_list.name"
+        :prepend-icon="nav_list.icon"
+        no-action
+        :append-icon="nav_list.lists ? undefind : ''"
+        >
+        <template v-slot:activator>
+        <v-list-item-content>
+          <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+        </v-list-item-content>
+        </template>
+          <v-list-item v-for="list in nav_list.lists" :key="list">
+            <v-list-item-content>
+              <v-list-item-title>{{list}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+      </v-container>
+    </v-navigation-drawer>
     <!-- 画面サイズによってサイドバーが表示・非表示される。ユーザーで操作するためにハンバーガーメニューを作る -->
     <!-- clippedを設定することで、ナビゲーションバーの下に表示する。-->
     <v-app-bar color="primary" clipped-left dark app>
@@ -38,7 +73,9 @@
       </v-menu>
     </v-toolbar-items>
     </v-app-bar>
-        
+        <v-main>
+          <router-view></router-view>
+        </v-main>
     <v-footer dark app>Vuetify</v-footer>
   </v-app>
 </template>
@@ -56,7 +93,36 @@ export default {
           {name: 'Github issue board',icon: 'mdi-github'},
           {name: 'Stack overview',icon: 'mdi-stack-overflow'},
           // mdi-のつけ忘れに注意
-        ]
+        ],
+        nav_lists:[
+        {
+          name: 'Getting Started',
+          icon: 'mdi-speedometer',
+          lists:['Quick Start','Pre-made layouts']
+        },
+        {
+          name: 'Customization',
+          icon: 'mdi-cogs' 
+        },
+        {
+          name: 'Styles & animations',
+          icon: 'mdi-palette',
+          lists:['Colors','Content','Display']
+        },
+        {
+          name: 'UI Components',
+          icon: 'mdi-view-dashboard',
+          lists:['API explorer','Alerts']
+        },
+        {
+          name: 'Directives',
+          icon: 'mdi-function'
+        },
+        {
+          name: 'Preminum themes',
+          icon: 'mdi-vuetify'
+        },
+      ]
     }
   },
 };
